68c98d152e77655c689e35f1bd5c879ecc8a8fe3
LucaHaverty <lucahaverty@gmail.com>
2023-01-08 17:10:50 -0800

    Added explosive enemy

--- Assets/Scrips/Enemies/Attacks/Types/BombAttack.cs
+++ Assets/Scrips/Enemies/Attacks/Types/BombAttack.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Health))]
 public class BombAttack : AbstractAttack
 {
+    public float range;
+
+    private bool exploded = false;
+
     protected override void Attack()
     {
-        target.Damage(damage);
+        if (exploded == true)
+        {
+            Debug.LogError("Exploded called more than once in BombAttack.cs");
+            return;
+        }
+
+        foreach (Transform building in Settings.instance.buildingContainer)
+        {
+            if (Vector2.Distance(transform.position, building.transform.position) > range)
+                continue;
+            
+            building.GetComponent<Health>().Damage(damage);
+        }
         GetComponent<Health>().InstaKill();
     }
 }
